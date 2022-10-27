fb72e7f6865e48c984f9db661945b3ea4a0dbedd
cbHasib <hasibul.pbn@gmail.com>
2022-10-27 20:43:04 +0600

    Added FAQs Data

--- src/Pages/FAQs/FAQs.js
+++ src/Pages/FAQs/FAQs.js
@@ -3,7 +3,6 @@ import useScrollToTop from "../../hooks/useScrollToTop";
 import useTitle from "../../hooks/useTitle";
 
 const FAQs = () => {
-
   useScrollToTop();
   useTitle("FAQs");
 
@@ -20,9 +19,7 @@ const FAQs = () => {
               <div class="divide-y divide-base-200 rounded-xl border border-base-200 bg-base-100">
                 <details class="group p-6" open>
                   <summary class="flex cursor-pointer items-center justify-between">
-                    <h2 class="text-lg font-medium">
-                      Lorem ipsum dolor sit amet consectetur adipisicing?
-                    </h2>
+                    <h2 class="text-lg font-medium">How to enroll a course?</h2>
 
                     <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
                       <svg
@@ -58,17 +55,15 @@ const FAQs = () => {
                   </summary>
 
                   <p class="mt-4 leading-relaxed">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
-                    hic veritatis molestias culpa in, recusandae laboriosam
-                    neque aliquid libero nesciunt voluptate dicta quo officiis
-                    explicabo consequuntur distinctio corporis earum similique!
+                    Go to course page then click on the 'Course Details' button
+                    that you want to enroll then enrolled that course.
                   </p>
                 </details>
 
                 <details class="group p-6">
                   <summary class="flex cursor-pointer items-center justify-between">
                     <h2 class="text-lg font-medium">
-                      Lorem ipsum dolor sit amet consectetur adipisicing?
+                      Is tha course Free or Paid?
                     </h2>
 
                     <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
@@ -105,69 +100,14 @@ const FAQs = () => {
                   </summary>
 
                   <p class="mt-4 leading-relaxed">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
-                    hic veritatis molestias culpa in, recusandae laboriosam
-                    neque aliquid libero nesciunt voluptate dicta quo officiis
-                    explicabo consequuntur distinctio corporis earum similique!
-                  </p>
-                </details>
-              </div>
-            </div>
-
-
-            <div class="max-w-lg">
-              <div class="divide-y divide-base-200 rounded-xl border border-base-200 bg-base-100">
-                <details class="group p-6" open>
-                  <summary class="flex cursor-pointer items-center justify-between">
-                    <h2 class="text-lg font-medium">
-                      Lorem ipsum dolor sit amet consectetur adipisicing?
-                    </h2>
-
-                    <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="2"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                      </svg>
-
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="2"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                      </svg>
-                    </span>
-                  </summary>
-
-                  <p class="mt-4 leading-relaxed">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
-                    hic veritatis molestias culpa in, recusandae laboriosam
-                    neque aliquid libero nesciunt voluptate dicta quo officiis
-                    explicabo consequuntur distinctio corporis earum similique!
+                    Both Free and Paid course are available at our website.
                   </p>
                 </details>
 
                 <details class="group p-6">
                   <summary class="flex cursor-pointer items-center justify-between">
                     <h2 class="text-lg font-medium">
-                      Lorem ipsum dolor sit amet consectetur adipisicing?
+                      Can you watch videos from my mobile device?
                     </h2>
 
                     <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
@@ -204,10 +144,54 @@ const FAQs = () => {
                   </summary>
 
                   <p class="mt-4 leading-relaxed">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
-                    hic veritatis molestias culpa in, recusandae laboriosam
-                    neque aliquid libero nesciunt voluptate dicta quo officiis
-                    explicabo consequuntur distinctio corporis earum similique!
+                    You can watch the videos from any device that connected to
+                    the internet from anywhere.
+                  </p>
+                </details>
+
+                <details class="group p-6">
+                  <summary class="flex cursor-pointer items-center justify-between">
+                    <h2 class="text-lg font-medium">
+                      I have forgotten my account password. What should I do?
+                    </h2>
+
+                    <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </span>
+                  </summary>
+
+                  <p class="mt-4 leading-relaxed">
+                    Simply go to login page then click on the 'Forget Password'.
+                    Submit your mail to get an email that contains recover
+                    password information.
                   </p>
                 </details>
               </div>
