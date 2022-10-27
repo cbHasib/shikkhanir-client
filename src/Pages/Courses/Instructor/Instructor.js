120f326aec8f31a449fb4caac710623fbd3586d9
cbHasib <hasibul.pbn@gmail.com>
2022-10-27 19:33:29 +0600

    Update Instructor Page

--- src/Pages/Courses/Instructor/Instructor.js
+++ src/Pages/Courses/Instructor/Instructor.js
@@ -1,45 +1,45 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 import useScrollToTop from "../../../hooks/useScrollToTop";
 import useTitle from "../../../hooks/useTitle";
+import bgCover from '../../../assets/images/instructorBG.svg'
 
 const Instructor = () => {
+  const { about, designation, image, name } = useLoaderData().data;
+
   useScrollToTop();
-  useTitle("Instructor");
+  useTitle(name);
   return (
-    <div className=" bg-gray-200 dark:bg-gray-900 flex flex-wrap items-center justify-center">
+    <div className=" bg-base-200 dark:bg-gray-900 flex flex-wrap items-center justify-center">
       <div className="container max-w-lg bg-white rounded dark:bg-gray-800 shadow-lg transform duration-200 easy-in-out m-12">
-        <div className="h-2/4 sm:h-64 overflow-hidden">
+        <div className="h-52 overflow-hidden">
           <img
-            className="w-full rounded-t object-cover sm:h-64"
-            src="https://firebasestorage.googleapis.com/v0/b/pshrd-d7d9f.appspot.com/o/shikkhanirServerWeb%2FinstructorImage%2FbgInstructor.jpg?alt=media&token=883e2307-30ea-4fc1-a182-5414d4a5b7b5"
+            className="w-full rounded-t object-cover"
+            src={bgCover}
             alt="PhotoUrl"
           />
         </div>
         <div className="flex justify-start px-5 -mt-12 mb-5">
-          <span clspanss="block relative h-32 w-32">
+          <span>
             <img
-              alt="PUnsplash"
-              src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              className="mx-auto object-cover rounded-full h-24 w-24 bg-white p-1"
+              alt={`${name} Visual`}
+              src={image}
+              className="mx-auto object-cover rounded-full h-24 w-24 bg-white p-1 border-2 border-primary"
             />
           </span>
         </div>
         <div className="">
           <div className="px-7 mb-8">
-            <h2 className="text-3xl font-bold text-green-800 dark:text-gray-300">
-              Beth J. Greene
+            <h2 className="text-3xl font-bold text-purple-700 dark:text-gray-300">
+              {name}
             </h2>
-            <p className="text-gray-400 mt-2 dark:text-gray-400">Illustrator</p>
-            <p className="mt-2 text-gray-600 dark:text-gray-300">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Asperiores molestiae vitae odio non commodi itaque quisquam
-              incidunt doloribus fugit nesciunt.
+            <p className="text-gray-400 mt-2 dark:text-gray-400">
+              {designation}
             </p>
-            <div className="justify-center px-4 py-2 cursor-pointer bg-green-900 max-w-min mx-auto mt-8 rounded-lg text-gray-300 hover:bg-green-800 hover:text-gray-100 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-gray-200">
-              bethgreene@gmail.com
-            </div>
+            <p className="mt-2 text-gray-600 dark:text-gray-300">{about}</p>
+
             <div className="flex flex-wrap justify-center gap-2 sm:gap-4 mt-8">
-              <button className="text-green-900 hover:text-green-700 p-1 sm:p-2 inline-flex items-center dark:text-gray-400 dark:hover:text-gray-300">
+              <button className="text-purple-900 hover:text-purple-700 p-1 sm:p-2 inline-flex items-center dark:text-gray-400 dark:hover:text-gray-300">
                 <svg
                   className="w-7 h-7 fill-current"
                   role="img"
@@ -49,7 +49,7 @@ const Instructor = () => {
                   <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
                 </svg>
               </button>
-              <button className="text-green-900 hover:text-green-700 p-1 sm:p-2 inline-flex items-center dark:text-gray-400 dark:hover:text-gray-300">
+              <button className="text-purple-900 hover:text-purple-700 p-1 sm:p-2 inline-flex items-center dark:text-gray-400 dark:hover:text-gray-300">
                 <svg
                   className="w-7 h-7 fill-current"
                   role="img"
@@ -59,7 +59,7 @@ const Instructor = () => {
                   <path d="M12 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0zm5.01 4.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 4.028 12c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM9.25 12C8.561 12 8 12.562 8 13.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.248.688 0 1.249-.561 1.249-1.249 0-.687-.562-1.249-1.25-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z" />
                 </svg>
               </button>
-              <button className="text-green-900 hover:text-green-700 p-1 sm:p-2  inline-flex items-center dark:text-gray-400 dark:hover:text-gray-30 dark:hover:text-gray-300">
+              <button className="text-purple-900 hover:text-purple-700 p-1 sm:p-2  inline-flex items-center dark:text-gray-400 dark:hover:text-gray-30 dark:hover:text-gray-300">
                 <svg
                   className="w-7 h-7 fill-current"
                   role="img"
@@ -69,7 +69,7 @@ const Instructor = () => {
                   <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 5.079 3.158 9.417 7.618 11.162-.105-.949-.199-2.403.041-3.439.219-.937 1.406-5.957 1.406-5.957s-.359-.72-.359-1.781c0-1.663.967-2.911 2.168-2.911 1.024 0 1.518.769 1.518 1.688 0 1.029-.653 2.567-.992 3.992-.285 1.193.6 2.165 1.775 2.165 2.128 0 3.768-2.245 3.768-5.487 0-2.861-2.063-4.869-5.008-4.869-3.41 0-5.409 2.562-5.409 5.199 0 1.033.394 2.143.889 2.741.099.12.112.225.085.345-.09.375-.293 1.199-.334 1.363-.053.225-.172.271-.401.165-1.495-.69-2.433-2.878-2.433-4.646 0-3.776 2.748-7.252 7.92-7.252 4.158 0 7.392 2.967 7.392 6.923 0 4.135-2.607 7.462-6.233 7.462-1.214 0-2.354-.629-2.758-1.379l-.749 2.848c-.269 1.045-1.004 2.352-1.498 3.146 1.123.345 2.306.535 3.55.535 6.607 0 11.985-5.365 11.985-11.987C23.97 5.39 18.592.026 11.985.026L12.017 0z" />
                 </svg>
               </button>
-              <button className="text-green-900 hover:text-green-700 p-1 sm:p-2  inline-flex items-center dark:text-gray-400 dark:hover:text-gray-30 dark:hover:text-gray-300">
+              <button className="text-purple-900 hover:text-purple-700 p-1 sm:p-2  inline-flex items-center dark:text-gray-400 dark:hover:text-gray-30 dark:hover:text-gray-300">
                 <svg
                   className="w-7 h-7 fill-current"
                   role="img"
@@ -81,7 +81,7 @@ const Instructor = () => {
                   </g>
                 </svg>
               </button>
-              <button className="text-green-900 hover:text-green-700 p-1 sm:p-2  inline-flex items-center dark:text-gray-400 dark:hover:text-gray-30 dark:hover:text-gray-300">
+              <button className="text-purple-900 hover:text-purple-700 p-1 sm:p-2  inline-flex items-center dark:text-gray-400 dark:hover:text-gray-30 dark:hover:text-gray-300">
                 <svg
                   className="w-7 h-7 fill-current"
                   xmlns="http://www.w3.org/2000/svg"
