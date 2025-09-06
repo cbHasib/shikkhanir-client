f121981a87b687419dc9e936bf49e746b11c9f13
Hasibul Hasan <hasibul.pbn@gmail.com>
2025-09-06 13:06:20 +0600

    Refactor useEffect dependencies in OpenApp component for clarity

--- src/Pages/OpenApp/OpenApp.js
+++ src/Pages/OpenApp/OpenApp.js
@@ -42,7 +42,8 @@ export default function OpenApp() {
 
     useEffect(() => {
         openApp(url, searchType)
-    }, [url, searchType, openApp])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [url, searchType])
 
 
 
